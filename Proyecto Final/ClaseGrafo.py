4b18d0f8a8935df0012acf873344514ef1dd5ebf
JulianMolina <73005582+Juleeeaaannn@users.noreply.github.com>
2022-11-24 07:36:08 -0300

    Add files via upload

--- Proyecto Final/ClaseGrafo.py
+++ Proyecto Final/ClaseGrafo.py
@@ -1,15 +1,71 @@
 import numpy as np
-from claseNodo import Nodo
+
 N=4
+
+class Nodo:
+    __arreglo = None #arreglo posee la reina
+    __dimension = None #dimension del arreglo
+    __pos = None #coordenadas reina
+    __ult=0 #ulitma reina
+    __cota=999
+    __sig=None
+
+    def __init__ (self, cant, x=None, y=None):
+        self.__dimension = cant
+        self.__arreglo = np.full(cant, -1)
+        self.__pos=[]
+        self.__sig=None
+        self.__cota=999
+        if x != None and y != None:
+            self.__pos.append(x)
+            self.__arreglo[x]=y
+    def setCota(self,cota):
+        self.__cota=cota
+    def getCota(self):
+        return self.__cota
+    def setArray(self,a):
+        self.__arreglo=a
+    def getArray(self):
+        return self.__arreglo
+    def setSig(self,elemento):
+        self.__sig=elemento
+    def setReina(self,x,y):
+        self.__pos.append(x)
+        self.__arreglo[x]=y
+        self.__ult+=1
+    def setReinas(self,a):
+        self.__pos=a
+    def getReinas(self):
+        return self.__pos
+    def getX(self):
+        if self.__pos != None:
+            return self.__pos[self.__ult]
+    
+    def getY(self):
+        if self.__pos!= None:
+            return self.__arreglo[self.__pos[self.__ult]]
+    def choca(self,reina): #trae solo una reina
+        choca=0
+        if len(reina.getReinas()) >= 1:
+            for x in self.__pos:
+                if abs(x+self.__arreglo[x]) != abs (reina.getX()+reina.getY()) and abs(x-self.__arreglo[x]) != abs(reina.getX()-reina.getY()):#diagonales
+                            if x != reina.getX() and self.__arreglo[x] != reina.getY():#vertical y horizontal
+                                pass
+                            else:
+                                choca+=1
+                else:
+                    choca +=1
+            return choca
+        else:
+            return choca
+
 class Grafo: #grafo conexo
-    __vertices = None
-    __matriz = None
+    __raiz=None
     __LNV=None #LISTA DE NODOS VIVOS
 
-    def __init__(self,vertices=[]):
-        self.__vertices=vertices
+    def __init__(self):
         self.__LNV=[]
-        self.__matriz=np.full(len(vertices),-1)
+        self.__raiz=None
 
     def Buscar(self, elemento):
         bandera = True
@@ -33,74 +89,55 @@ class Grafo: #grafo conexo
         self.__LNV.append(new1)
         self.__LNV.append(nuevo)
         self.__LNV.append(new3)
-            
-    def NodoPrometedor(self,nivel,padre):
-        min=99
-        for i in self.__vertices:
-            if i.getY()==nivel:
-                if i.choca(padre) < min: 
-                    min = i.Colision(self.__LNV)
-                    nodo=i
-        return nodo
-        
-    def BuscarHijo(self,nivel):
+
+
+    def Genera(self,nivel):
         lista=[]
-        print("-------hijos--------")
-        for i in self.__vertices:
-            if i.getY()==nivel:
-                print("x:",i.getX(),"y:",i.getY())
-                lista.append(i)
-        print("--------------")
+        i=0
+        for i in range(N):
+                new=Nodo(N,i,nivel)
+                lista.append(new)
         return lista
 
-    '''
-    def Arbol(self,nivel):
-        if nivel < N:
-            for j in self.__LNV:
-                print("nivel:",nivel)
-                listaHijos=self.BuscarHijo(nivel)
-                listah=[]
-                for i in listaHijos:
-                    print("Nodo Hijo",i.choca(j))
-                    if i.choca(j) == 0:
-                        i.setReina(j.getX(),j.getY())
-                        listah.append(i)
-            self.__LNV+=listah
-            self.Arbol(nivel+1)
-        else:
-            print(self.Mostrar())
-    '''
+    def BuscarHijo(self,nivel):
+        lista=self.Genera(nivel)
+        print(lista)
+        return lista
+
     def BuscarLNV(self,elemento):
         i=0
         while(i<len(self.__LNV) and elemento!=self.__LNV[i]):
             i+=1
-        if i<len(self.__LNV):
-            print("i:",i)
+        if i<len(self.__LNV) and elemento==self.__LNV[i]:
             return i
         else:
             print("No se encontro el elemento en la LNV")
 
     def Arbol(self):
         k=0
-        for nivel in range(1,4,+1):
-            while k < len(self.__LNV):
-                self.__LNV
-                j=self.__LNV[0]    
-                print("LNV :",self.__LNV)
-                print("nivel:",nivel)
-                print(j)
-                print("padre:",j.getX(),j.getY())
-                listaHijos=self.BuscarHijo(nivel)
-                listah=[]
-                for i in listaHijos:
-                    print("grado:",i.choca(j))
-                    if i.choca(j)==0:
-                        aux=i
-                        aux.setReina(j.getX(),j.getY())
-                        listah.append(aux)
-                self.__LNV.pop(0)
-                self.__LNV+=listah
+        for nivel in range(1,4):
+            for j in self.__LNV:
+                    listah=[]
+                    print("-------------") 
+                    print("nivel:",nivel)
+                    print("padre:",j.getX(),j.getY())
+                    listaHijos=self.BuscarHijo(nivel)
+                    listah.append(self.Asociar(listaHijos,j))
+            self.__LNV.pop(self.BuscarLNV(j))
+            self.__LNV+=listah
 
+    def Asociar(self,hijos,padre):
+        for i in hijos:
+            print("hijo;x y:",i.getX(),i.getY(),"colision:",padre.choca(i))
+            if i.choca(padre)==0 and i.choca(padre)<=padre.getCota():
+                print("nodos vivos",i.getX(),i.getY())
+                padre.setReina(i.getX(),i.getY())
+                a=Nodo(N)
+                nuevo=self.Copiar(a,i)
+                return padre
+    def Copiar(self,A,B):
+        A.setArray(B.getArray())
+        A.setReinas(B.getReinas)
     def Mostrar(self):
         for i in self.__LNV:
             print("lista de nodos vivos----------")
@@ -130,12 +167,7 @@ hasta que la lista esta vacia
  '''
 
 if __name__ == '__main__':
-    lista=[]
-    for i in range(N):
-        for j in range(N):
-            new=Nodo(N,i,j)
-            lista.append(new)
-    grafo=Grafo(lista)
+    grafo=Grafo()
     grafo.CrearNodoInicial()
     grafo.Arbol()
     
